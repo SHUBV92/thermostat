29423a39e7efcf80761800e337707c34f8d6429d
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-11-15 17:58:10 +0000

    Thermostat Api Called

--- interface.js
+++ interface.js
@@ -48,17 +48,17 @@ $(document).ready(function() {
     $('#energy').text(thermostat._energyUsage);
   };
 
-  // $('#current-city').change(function(){ 
-  //     var city = $('#current-city').val();
-  // $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-  //   $('#current-temperature').text(data.main.temp);
+  $('#current-city').change(function(){ 
+      var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
 
-  function displayWeather(city) {
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
-    var units = '&units=metric';
-    $.get(url + token + units, function(data) {
-      $('#current-temperature').text(data.main.temp);
-    });
-    };
+  // function displayWeather(city) {
+  //   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  //   var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+  //   var units = '&units=metric';
+  //   $.get(url + token + units, function(data) {
+  //     $('#current-temperature').text(data.main.temp);
+  });
+   });
   });
